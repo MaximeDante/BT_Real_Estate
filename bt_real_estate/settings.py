68466b795d7d8fbf1a85226d75d9f3312979a928
MAXIMEDANTE\Owner <maximekanyinda@gmail.com>
2021-05-15 16:47:04 +0200

    add realtor and listings model, run migration and add initial data

--- bt_real_estate/settings.py
+++ bt_real_estate/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'bt_real_estate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'maxime',
+        'HOST': 'localhost',
     }
 }
 
@@ -128,6 +131,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bt_real_estate/static')
 ]
+
+# Media Folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
