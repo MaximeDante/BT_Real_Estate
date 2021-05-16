090d3fd61a59f989e0f34884c0dd5ed70c6d8573
MAXIMEDANTE\Owner <maximekanyinda@gmail.com>
2021-05-16 17:09:18 +0200

    implement search functionality

--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,71 @@
-from django.shortcuts import render
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 2)
+    page = request.GET.get('page')
+    page_listings = paginator.get_page(page)
+    context = {
+        'listings': page_listings,
+    }
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(title__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State
+    if 'province' in request.GET:
+        province = request.GET['province']
+        if province:
+            queryset_list = queryset_list.filter(province__iexact=province)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
